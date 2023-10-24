bfe1995eda63d190224f32024c8842e8b8d72a49
edumm <192666@unsaac.edu.pe>
2023-10-23 23:51:01 -0500

    create new proyect

---
+++ app/(main)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { NavigationHeader } from "@/components/navigation/navigation-header";
+
+const Home = () => {
+  return ( 
+    <>
+      <NavigationHeader />
+      <ModeToggle />
+    </> 
+   );
+}
+ 
+export default Home;
