6b2a036f7f380fbde84cfcebf603a25a28179a29
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-05-31 23:12:58 -0500

    add changes

--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -1,11 +1,13 @@
 import {NavigationMenuDemo} from "@/components/nav/navigator";
+import {ModeToggle} from "@/components/button-theme";
 
 const MainLayout = async ({children}: { children: React.ReactNode }) => {
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-full relative">
             <main className="">
                 {children}
             </main>
+            <ModeToggle className='absolute t-1'/>
         </div>
     );
 }
