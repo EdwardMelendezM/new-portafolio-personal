4da1b8a579ec07ff768fa7dbc88f5cd71ded7433
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-06-02 18:18:11 -0500

    add changes

--- cicd/gen-version.sh
+++ cicd/gen-version.sh
@@ -6,11 +6,13 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 # Validate the current branch
 if [ "$current_branch" != "develop" ] && [ "$current_branch" != "main" ]; then
     echo "Error: Este script solo debe ejecutarse en las ramas 'develop' o 'main'. Rama: $current_branch"
-    exit 0
+    exit 1
 fi
 
 # Get the latest tag
 latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1) | sed 's/v//g')
+echo "Last tag"
+echo "v$latest_tag"
 
 # If there are no tags, set the next version to v1.0.0
 if [ -z "$latest_tag" ]; then
@@ -20,24 +22,24 @@ else
     major=$(echo $latest_tag | cut -d. -f1)
     minor=$(echo $latest_tag | cut -d. -f2)
     patch=$(echo $latest_tag | cut -d. -f3)
-
     # Increment the version based on the current branch DEVELOP
-    if [ "$current_branch" == "develop" ]; then
+    if [ "$current_branch" = "develop" ]; then
         minor=$((minor + 1))
         patch=0
     fi
 
     # Increment the version based on the current branch MAIN
-    if [ "$current_branch" == "main" ]; then
+    if [ "$current_branch" = "main" ]; then
         major=$((major + 1))
         minor=0
         patch=0
     fi
 
     # Set the next version
-    next_version="v$major.$minor.$((patch))"
+    next_version="v$major.$minor.$patch"
 
 fi
 
 # Output the next version
+echo "New tag"
 echo "$next_version"
