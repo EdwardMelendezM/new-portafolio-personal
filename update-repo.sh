b354ac662b55c3fa568f13d14b75f66c19fa84e8
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-06-02 21:53:28 -0500

    add changes

---
+++ update-repo.sh
@@ -0,0 +1,1 @@
+/home/ubuntu/.nvm/versions/node/v20.9.0/bin/npm run build && /home/ubuntu/.nvm/versions/node/v20.9.0/bin/pm2 restart portafolio-app || /home/ubuntu/.nvm/versions/node/v20.9.0/bin/pm2 start npm --name "portafolio-app" -- run start
