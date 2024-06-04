6d4c4c640e88d018cfc6c39d79abdf40e5767879
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-06-03 21:43:57 -0500

    add changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-pwd &&
+pwd
+# Cargar NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Verificar versiones
+npm -v
+pm2 -v
+
 /home/ubuntu/.nvm/versions/node/v20.9.0/bin/npm run build &&
  /home/ubuntu/.nvm/versions/node/v20.9.0/bin/pm2 restart portafolio-app || /home/ubuntu/.nvm/versions/node/v20.9.0/bin/pm2 start npm --name "portafolio-app" -- run start
